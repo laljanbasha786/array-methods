d36ccc20bf91660bf7935b266a9628363f9ca243
laljanbasha786 <laljanbasha04@gmail.com>
2024-11-07 14:53:20 +0530

    array methods

---
+++ day1.js
@@ -0,0 +1,38 @@
+//1.A
+
+//2.const a=[2,2,4];
+// const res=a.map(i=>i*3)
+// console.log(res);
+
+// 3.A
+
+
+//1.B
+// 2.let a=[1,2,3,4,5]
+// let b=a.filter(i=>i%2===0)
+// console.log(b)
+
+// 3.let a=[1,2,3]
+// let res=a.filter(num=>num>4)
+// console.log(res);
+//o/p-[]
+
+//1.B
+//2. var a=[5,10,15]
+//  var res=a.includes(10)
+// console.log(res);
+// 3.B
+
+//1.A
+//2. var a=[10,15,25,30]
+// var res=a.find(num=>num>20)
+// console.log(res);
+//3.A
+
+//1.A
+//2.  var arr=[1, [2, [3, 4]], 5]
+//  var res=arr.flat()
+//  console.log(res);
+//  3.A
+
+
